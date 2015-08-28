ccb4c0bf5a0536c71955b56e36c64f041a83e92b
Roy Crihfield <rscrihf@gmail.com>
2015-08-27 23:01:44 -0500

    idk

--- fetch.py
+++ fetch.py
@@ -16,32 +16,44 @@ acr_url = 'https://apps.fsa.usda.gov/acr/'
 download_root = './'
 
 class Fetcher:
-  
-  def __init__(self):
-
-    profile = webdriver.FirefoxProfile()
-    profile.set_preference('browser.download.folderList', 2)
-    profile.set_preference('browser.download.manager.showWhenStarting', False)
-    profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'text/csv')
-    profile.set_preference('browser.helperApps.neverAsk.openFile', 'text/csv')
-    profile.update_preferences()
     
-    self._dr = webdriver.Firefox(profile)
-    self._wait = WebDriverWait(self._dr, 10)
+  def __init__(self, dlpath):
+    self._dr = Fetcher._driver(dlpath)
+    self.set_wait(10)
     
   def set_wait(self, n):
     self._wait = WebDriverWait(self._dr, n)
 
+  def _driver(dlpath):
+    # profile = webdriver.FirefoxProfile()
+    # profile.set_preference('browser.download.folderList', 2)
+    # profile.set_preference('browser.download.manager.showWhenStarting', False)
+    # profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'text/csv')
+    # profile.set_preference('browser.helperApps.neverAsk.openFile', 'text/csv')
+    # profile.update_preferences()
+    # return webdriver.Firefox(profile)
+    options = webdriver.ChromeOptions()
+    prefs = {'download.default_directory': dlpath}
+    options.add_experimental_option('prefs', prefs)
+    return webdriver.Chrome(chrome_options=options)
+
+  def set_dlpath(self, path):
+    # self._dr.profile.set_preference('browser.download.dir', path)
+    # self._dr.profile.update_preferences()
+    pass
+    
   # Returns true if page was refreshed
   def _wait_for_counties(self, elt_county):
+    reloaded = False
     try:
       self._wait.until(EC.staleness_of(elt_county))
-      return False
     except sexc.TimeoutException as e:
       # if it timed out, maybe needs to "refresh"
       self._dr.execute_script('submitRequest("displayParameters")')
       self._wait.until(EC.staleness_of(elt_county))
-      return True
+      reloaded = True
+    self._wait.until(EC.presence_of_element_located((By.ID, 'county')))
+    return reloaded
     
   def _get_homepage(self):
     self._dr.get(acr_url)
@@ -73,6 +85,8 @@ class Fetcher:
     return by_value, by_name
 
   def request_data(self, state, county, year):
+    debug('request_data:', county_codes.state_names[state], county, year)
+    
     self._get_homepage()
     
     elt_state = self._dr.find_element_by_id('state')
@@ -105,26 +119,37 @@ class Fetcher:
     elt_commodity = self._dr.find_element_by_id('commodity')
     Select(elt_commodity).select_by_visible_text('CORN')
 
-    # btn_run = self._dr.find_element_by_xpath('//*[@title="Run Report"]')
-    # btn_run.click()
     self._dr.execute_script('submitRequest("displayReport")')
 
     # Get CSV file
-    dl_path = os.path.join(download_root, str(state), str(county))
-    os.makedirs(dl_path, exist_ok=True)
-
-    self._dr.profile.set_preference('browser.download.dir', dl_path)
-    self._dr.profile.update_preferences()
-
-    # xpath_getcsv = '//*[@title="Comma-Separated Values"]'
-    # self._wait.until(EC.presence_of_element_located((By.XPATH, xpath_getcsv)))
+    # dlpath = os.path.join(download_root, str(state), str(county))
+    # os.makedirs(dlpath, exist_ok=True)
+    # self.set_dlpath(dlpath)
+    
+    xpath_getcsv = '//*[@title="Comma-Separated Values"]'
+    self._wait.until(EC.presence_of_element_located((By.XPATH, xpath_getcsv)))
     self._dr.execute_script('submitRequest("exportToCSV")')
 
-  def request_all_counties(self, state):
+  def request_all_counties(self, state, c=None):
     name = county_codes.state_names[state]
-    for county in county_codes.counties[name]:
+    if c is None:
+      counties = county_codes.counties[name]
+    elif isinstance(c, str):
+      counties = [cty for cty in county_codes.counties[name]
+                  if c <= cty]
+    debug("counties:", counties)
+    for county in counties:
       self.request_all_years(state, county)
 
   def request_all_years(self, state, county):
     for year in range(2004, 2009):
-      self.request_data(state, county, y)
+      attempts = 5
+      while attempts > 0:
+        try:
+          self.request_data(state, county, year)
+          break
+        except sexc.TimeoutException as e:
+          attempts -= 1
+          # if not attempts:
+          #   debug("max attempts made:", state, county)
+          #   raise e
