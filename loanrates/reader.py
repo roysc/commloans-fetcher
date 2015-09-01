2b0c3f3560ac1a32e0c6336cabfb41238cfc2ef7
Roy Crihfield <rscrihf@gmail.com>
2015-09-01 00:05:48 -0400

    process csv files

--- loanrates/reader.py
+++ loanrates/reader.py
@@ -1,5 +1,6 @@
+import os
 import pandas as pd
-import decimal
+from loanrates import county_codes
 
 column_names = [
   'commodity',
@@ -12,16 +13,81 @@ column_names = [
   'effective_acre_ldp',
 ]
 
+# keep_columns = (
+# 'loan_rate', 'effective_pcp', 'effective_ldp'
+# )
+
+drop_columns = [
+  # Commodity always == "Corn"
+  'commodity',
+  # These are always(?) NaN
+  'pcp_30day',
+  'pcp_alternative',
+  'effective_acre_ldp',
+]
+
 def read_csv(fp):
+  from itertools import islice
+
   d = pd.read_csv(fp,
                   skiprows=4,
                   parse_dates=['Effective Date'])
   d.columns = column_names
-  # Commodity always == Corn
-  d = d.drop('commodity', axis=1)
-
-  for col in ('loan_rate', 'effective_pcp', 'effective_ldp'):
-    d[col] = d[col].str.strip('$ ').astype(float)
-
   d.set_index('date', inplace=True)
+
+  # Drop unneeded columns
+  d = d.drop(drop_columns, axis=1)
+
+  # Strip $, convert to float
+  def parsemoney(c):
+    return c.str.strip('$ ').astype(float)
+  d = d.apply(parsemoney)
+  
   return d
+
+def get_state_county(fp):
+  fp = open(path)
+  descr = next(islice(fp, 3, None))
+  # ...
+
+
+class Reader:
+  def __init__(self, root):
+    self.root= root
+
+
+  def process_all_files(self, state, county):
+    path = os.path.join(self.root, state, county)
+
+    dfs = []
+    for fn in os.listdir(path):
+      fpath = os.path.join(path, fn)
+      print("reading:", fpath)
+      d = read_csv(fpath)
+      # Use multi-index for better organization
+      index = pd.MultiIndex.from_product([[state], [county], d.columns])
+      d.columns = index
+      assert not d.isnull().any().any(), d
+      dfs.append(d)
+
+    ret = pd.concat(dfs)
+    # Drop duplicates
+    ret = ret.groupby(level=0).first()
+    ret.sort(inplace=True)
+    return ret
+
+  def process_all_counties(self, state):
+    path = os.path.join(self.root, state)
+    statename = county_codes.state_names[state]
+    counties = county_codes.counties[statename]
+    dfs = []
+    for fn in os.listdir(path):
+      if fn not in counties:
+        print('bad county code:', fn)
+      d = self.process_all_files(state, fn)
+      assert not d.isnull().any().any(), d
+      dfs.append(d)
+
+    ret = pd.concat(dfs, axis=1)
+    ret.sort(axis=1, inplace=True)
+    return ret
