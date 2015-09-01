92ae7150eae1999726e9d5e1fbd363b56eece7e7
Roy Crihfield <rscrihf@gmail.com>
2015-08-31 22:22:50 -0400

    useless pop data parser

--- pop/reader.py
+++ pop/reader.py
@@ -1,11 +1,22 @@
 import pandas as pd
 
-def read_csv(fp):
+def read_csv_pop2(fp):
 
   d = pd.read_csv(fp, header=3)
   # rename first column
   d.rename(columns={d.columns[0]: 'CDP'}, inplace=True)
+  d.set_index('CDP', inplace=True)
   # Drop NaNs 
-  d.dropna()
+  d.dropna(inplace=True)
   # Drop state-level data
-  d.drop(d.index[0])
+  d.drop(d.index[0], inplace=True)
+  # Drop "Estimates Base" and "Census"
+  d.drop(d.columns[-2:], axis=1, inplace=True)
+
+  # Remove commas and convert to numeric value
+  def nocomma(s):
+    r = s.str.replace(',','')
+    return r.astype(int)
+  d = d.apply(nocomma)
+
+  return d
