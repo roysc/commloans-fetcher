6ceca8529741f584cb895ed9b767fb465007bb06
Roy Crihfield <rscrihf@gmail.com>
2015-09-04 17:35:37 -0400

    read harvest dates

--- commloans/misc.py
+++ commloans/misc.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from datetime import datetime, timedelta
 
+from commloans import county_codes as cc
+
 def ez_read_csv(f):
   d = pd.read_csv(f, header=[0,1], parse_dates=0, index_col=0)
   levs = d.columns.levels
@@ -25,3 +27,30 @@ def yearly_dates(ix, startmd, endmd):
   for y in years:
     ret = ret | getdates(y)
   return ret
+
+def read_harvest_dates(f):
+
+  def splitdm(col):
+    s = col.str.strip()
+    s = s.str.split()
+    l = []
+    for m, d in s:
+      l.append((datetime.strptime(m, '%b').month, int(d)))
+    return pd.Series(l, index=s.index)
+
+  d = pd.read_csv(f,index_col=0)
+  start = splitdm(d['start'])
+  end = splitdm(d['end'])
+  ranges = pd.concat({'start':start,'end':end},axis=1)
+  
+  return ranges
+
+
+def ez_harvest_price_mean(alldata, st, h):
+  
+  dates = h.loc[st]
+  d = alldata[st]
+  i = yearly_dates(d.index, dates['start'], dates['end'])
+  d = d.ix[i]
+  g = d.groupby(d.index.year)
+  return g.mean()
